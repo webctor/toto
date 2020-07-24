e9f830946af94b6a24d27c8543c7f96f393ff170
Hector Acosta <hectoracosta@me.com>
2020-07-24 12:55:32 -0700

    Initial Commit

---
+++ src/main/java/com/rubyslippers/toto/TotoApplication.java
@@ -0,0 +1,13 @@
+package com.rubyslippers.toto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TotoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rubyslippers/toto/TotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubyslippers.toto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
